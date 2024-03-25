22aa538db56538dec0d345ba741d3dd9f7574b25
merk0n <127611734+Merk0n@users.noreply.github.com>
2024-03-25 17:06:44 +0100

    Update output path in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,7 @@
-const path = require('path');
-
 module.exports = {
   entry: './src/script.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/weather-app/',
+    path: '/',
   },
 };
