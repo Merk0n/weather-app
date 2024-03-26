2678c22413870ac87eed91e796f3738b4e688e00
merk0n <127611734+Merk0n@users.noreply.github.com>
2024-03-26 22:42:55 +0100

    Add predeploy script and update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 module.exports = {
   entry: './src/script.js',
   output: {
     filename: 'main.js',
-    path: '/',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/weather-app/',
   },
 };
